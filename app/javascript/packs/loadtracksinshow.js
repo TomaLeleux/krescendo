2a38c7fc0648a8ea23bc9971f3641c582edd04f1
Maxime Favret <favretmaxime@gmail.com>
2018-06-12 11:07:47 +0200

    fixed reload and first load in show

--- app/javascript/packs/loadtracksinshow.js
+++ app/javascript/packs/loadtracksinshow.js
@@ -28,22 +28,18 @@ const buildHref = (href, id) => {
   return (newHref);
 };
 
+const addListenerToButton = (id, body, modal) => {
+  for (let i = 0; i < body.length; i++) {
+    const newLink = buildHref(body[i].getAttribute('href'), id);
+    body[i].setAttribute('href', newLink);
+  };
+  modal.style.display = "block";
+};
+
 const addListenersForPlaylist = () => {
-  const buttons = document.querySelectorAll('.fa-plus');
   const modal = document.getElementById('playlist-modal');
   const span = document.getElementsByClassName("close")[0];
-  const body = modal.querySelector('.modal-body').children;
 
-  buttons.forEach((button) => {
-    const id = button.parentElement.parentElement.id;
-    button.onclick = function() {
-      for (let i = 0; i < body.length; i++) {
-        const newLink = buildHref(body[i].getAttribute('href'), id);
-        body[i].setAttribute('href', newLink);
-      };
-      modal.style.display = "block";
-    }
-  });
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
     modal.style.display = "none";
@@ -61,7 +57,8 @@ if (document.querySelector('.album-id')){
   const idAlbum =  document.querySelector('.album-id').dataset.album
   let i = 'first'
   const targetTrack = document.querySelector(".first-track")
-  console.log(idAlbum)
+  console.log(idAlbum);
+  addListenersForPlaylist();
     // axios.get(`/albumtracks/${parseInt(idAlbum)}`  ,{responseType:'json'})
     axios({
       method:'get',
@@ -69,6 +66,8 @@ if (document.querySelector('.album-id')){
     })
     .then(function (response) {
       response.data[parseInt(idAlbum)].forEach(function(element) {
+        const modal = document.getElementById('playlist-modal');
+        const body = modal.querySelector('.modal-body').children;
         if (i == 'first'){
           const searchPlayer = element['artist']['name'].toLowerCase().split(' ').join('+')+'+'+element['title_short'].toLowerCase().split(' ').join('+');
           //add in video the first track
@@ -81,13 +80,20 @@ if (document.querySelector('.album-id')){
           targetTrack.setAttribute('id',`${element['id']}`);
           targetTrack.setAttribute('data-artist',`${element['artist']['name']}`)
           targetTrack.setAttribute('data-song',`${element['title_short']}`)
+          targetTrack.insertAdjacentHTML('beforeend', '  <a style="cursor: pointer;"><i class="fa fa-plus" aria-hidden="true"></i></a>');
+          targetTrack.lastChild.onclick = function () {
+            addListenerToButton(element['id'], body, modal);
+          };
           document.querySelector('.track-name').innerText = `${element['title_short']}`;
           document.getElementById(`${element['id']}`).parentNode.querySelector('.fa').addEventListener('click', trackRefresh, false);
           i = '';
         }else{
           //add in track list the other tracks with class active
-          document.getElementById('tracklist').insertAdjacentHTML('beforeend',`<li><span class="other-track" id="${element['id']}" data-artist="${element['artist']['name']}" data-song="${element['title_short']}"></span><i class="fa fa-play-circle" aria-hidden="true"></i><span class="track-name">${element['title_short']}</span></li>`)
+          document.getElementById('tracklist').insertAdjacentHTML('beforeend',`<li><span class="other-track" id="${element['id']}" data-artist="${element['artist']['name']}" data-song="${element['title_short']}"></span><i class="fa fa-play-circle" aria-hidden="true"></i><span class="track-name">${element['title_short']}</span>  <a style="cursor: pointer;"><i class="fa fa-plus" aria-hidden="true"></i></a></li>`)
           document.getElementById(`${element['id']}`).parentNode.querySelector('.fa').addEventListener('click', trackRefresh, false);
+          document.getElementById(`${element['id']}`).parentNode.lastChild.onclick = function () {
+            addListenerToButton(element['id'], body, modal);
+          };
         }
       });
       //add in lyrics the first track
@@ -105,12 +111,6 @@ if (document.querySelector('.album-id')){
     })
     .catch(function (error) {
       console.log(error);
-    })
-    .then(function () {
-      addButtonToPlaylist();
-    })
-    .then(function () {
-      addListenersForPlaylist();
     });
   }
 
