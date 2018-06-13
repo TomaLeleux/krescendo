82f53b26d1e327809564ada5536cd1a25fb0da25
MartinLNor <mlemiere@norauto.fr>
2018-06-13 23:12:52 +0200

    Add metadata on the website following Wagin tutorial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :authenticate_user!
 
-   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
@@ -23,6 +23,10 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+    # heroku config:set HOST=www.my_product.com
+  end
 
   private
 
