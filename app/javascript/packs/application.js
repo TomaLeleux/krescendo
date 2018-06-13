9487f6c2af907c561dc4b278f94c6f6c3a1b67b4
Maxime Favret <favretmaxime@gmail.com>
2018-06-13 13:41:26 +0200

    before merge

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,12 +5,7 @@ import './lyrcisscroll.js';
 if (document.querySelectorAll('.playlist .flex-container')) {
   listenToPlaylist();
 }
+
 import './loadtracksinshow.js'
-
-
-// if (document.getElementById('tracklist')) {
-//   addButtonToPlaylist();
-// }
-
 import './reloadshow.js'
 import './mobile_swipe.js'
